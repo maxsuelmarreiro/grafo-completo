d51866689423a0134cf45735c9be32785b54ad92
fran.weidt <fran.weidt@8dc69b2d-6ea1-864e-b670-4199812cdfed>
2011-06-16 03:13:49 +0000

    Para começar... coloquei no main um código que lê o txt e coloca em um array as arestas...obviamente não vai ficar lá...mas como ainda não tem as classes deixei lá.

---
+++ Grafos/src/grafos/Grafo.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package grafos;
+
+/**
+ *
+ * @author Fran
+ */
+public class Grafo {
+
+}
---
+++ Grafos/src/grafos/Main.java
@@ -0,0 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package grafos;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+/**
+ *
+ * @author Fran
+ */
+public class Main {
+
+
+
+   
+    public static void main(String[] args) {
+        String linha = null;
+         
+
+      try {
+         FileReader reader = new FileReader("grafo.txt");
+         BufferedReader leitor = new BufferedReader(reader);
+         StringTokenizer vert_peso = null;
+         StringTokenizer vert = null;
+
+
+         while ((linha = leitor.readLine()) != null) {
+
+
+            vert_peso = new StringTokenizer(linha, ": -");
+
+            ArrayList<String> aresta =new ArrayList<String>();
+            
+            while (vert_peso.hasMoreTokens()) {
+
+               // Campo 1
+               aresta.add(vert_peso.nextToken());
+               vert = new StringTokenizer(linha, "-");
+         
+            }
+            System.out.println(aresta);
+         }
+         leitor.close();
+         reader.close();
+
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+    }
+
+}
