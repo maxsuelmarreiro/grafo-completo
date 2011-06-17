4ac99857d2281654c3413054fc00f06db246d046
rookejf <rookejf@8dc69b2d-6ea1-864e-b670-4199812cdfed>
2011-06-17 11:51:15 +0000

    Estruturas prontas

--- Grafos/src/grafos/Grafo.java
+++ Grafos/src/grafos/Grafo.java
@@ -5,10 +5,31 @@
 
 package grafos;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Fran
  */
 public class Grafo {
+    private ArrayList<Vertice> vertices;
+    private boolean orientado;
 
+    public boolean isOrientado() {
+        return orientado;
+    }
+
+    public void setOrientado(boolean orientado) {
+        this.orientado = orientado;
+    }
+
+    public ArrayList<Vertice> getVertices() {
+        return vertices;
+    }
+
+    public void setVertices(ArrayList<Vertice> vertices) {
+        this.vertices = vertices;
+    }
+
+    
 }
---
+++ Grafos/src/grafos/Vertice.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package grafos;
+
+/**
+ *
+ * @author rooke
+ */
+public class Vertice {
+    private Vertice adjacente;
+    private int id;
+    private int peso;
+
+    public Vertice getAdjacente() {
+        return adjacente;
+    }
+
+    public void setAdjacente(Vertice adjacente) {
+        this.adjacente = adjacente;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    
+}
